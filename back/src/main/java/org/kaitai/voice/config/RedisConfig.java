a5fc44993e60e20937dcc02e4f00c7346b050c43
Your Name <you@example.com>
2026-02-10 19:22:04 +0500

    9

--- back/src/main/java/org/kaitai/voice/config/RedisConfig.java
+++ back/src/main/java/org/kaitai/voice/config/RedisConfig.java
@@ -1,35 +1,37 @@
-// package org.kaitai.voice.config;
+package org.kaitai.voice.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.data.redis.connection.RedisConnectionFactory;
-// import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-// import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-// import org.springframework.data.redis.core.RedisTemplate;
-// import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-// import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-// @Configuration
-// public class RedisConfig {
+@Configuration
+public class RedisConfig {
 
-//     @Bean
-//     public RedisConnectionFactory redisConnectionFactory() {
-//         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-//         config.setHostName("localhost");
-//         config.setPort(6379);
-//         return new LettuceConnectionFactory(config);
-//     }
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName("redis");
+        config.setPort(6379);
+        config.setPassword("redis123");  // ← добавьте пароль если есть
 
-//     @Bean
-//     public RedisTemplate<String, Object> redisTemplate() {
-//         RedisTemplate<String, Object> template = new RedisTemplate<>();
-//         template.setConnectionFactory(redisConnectionFactory());
-//         template.setKeySerializer(new StringRedisSerializer());
-//         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-//         template.setHashKeySerializer(new StringRedisSerializer());
-//         template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-//         template.setEnableTransactionSupport(true);
-//         template.afterPropertiesSet();
-//         return template;
-//     }
-// }
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setEnableTransactionSupport(true);
+        template.afterPropertiesSet();
+        return template;
+    }
+}
